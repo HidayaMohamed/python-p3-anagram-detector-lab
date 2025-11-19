f0aacada1d770167b854d3723dd5e044ad05961f
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-19 07:16:19 +0300

    Completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+
+        matches = []
+        for w in word_list:
+            if w.lower() != self.word and sorted(w.lower()) == sorted_word:
+                matches.append(w)
+
+        return matches
